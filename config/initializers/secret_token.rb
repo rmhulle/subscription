d5aec627b60450ad9b28d150f107818fa2011bd2
Marcelo Paez Sequeira <nkr@Casa-do-Capeta.local>
2014-04-25 08:40:18 -0300

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IuguSubscriptionExample::Application.config.secret_token = '98618339e7bc88f0bdcf1ccc94a5c2aeb377fd3b269413ab57c09b671ace70f25a06c83d277a691875490692347bf24a5e14f4691778830dcb952f92f04736a4'
